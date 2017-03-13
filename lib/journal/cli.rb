c07a41562451a948378cacc4a0c6db8303fbfa13
Fredrik Möllerstrand <fredrik@mollerstrand.se>
2017-03-13 16:29:02 +0100

    Commit journal page after editing

--- lib/journal/cli.rb
+++ lib/journal/cli.rb
@@ -5,15 +5,24 @@ module Journal
     def run
       usage_and_quit unless valid_day?(day)
 
-      edit_day(day)
-      # ..and git commit?!
-    end
-
-    def edit_day(day)
       date = Journal::Names.day_to_date(day)
       filename = Journal::Names.filename_for_date(date)
 
+      edit_journal_page(filename)
+      commit_journal_page(filename, date)
+    end
+
+    def edit_journal_page(filename)
       system("#{ENV['EDITOR']} #{filename}")
+    end
+
+    def commit_journal_page(filename, date)
+      # If the file has changed, we should commit it.
+      # The message is the weekday, e.g. 'monday'.
+      # We might want to add a qualifier too, like 'monday morning' or 'friday afternoon'.
+      repo = Journal::Configuration::JOURNAL_PATH
+      message = date.strftime("%A").downcase
+      system("cd #{repo} && git add #{filename} && git commit #{filename} -m #{message}")
     end
 
     private
